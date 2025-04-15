bc06331a1266c799d724866cace3a3ea64beb8d6
jan-laepple <jan.laepple@gmail.com>
2025-04-15 20:03:24 +0200

    added yaw degree to vector conversion

--- Entities/Scenario.gd
+++ Entities/Scenario.gd
@@ -25,21 +25,24 @@ func _init(level_name,dict : Dictionary):
 	Objects = HangarObject.generateHangarObjectsFromArray(objects)
 	#Objects = HangarObject.new(objects)
 	var properties = get_property_list()
-	add_barrier_wall(Objects, Vector3(11,0,-6),0,0)
-	add_barrier_wall(Objects, Vector3(10,0,-5),1,1)
+	add_barrier_wall(Objects, Vector3(11,0,-6),135)
+	add_barrier_wall(Objects, Vector3(11,0,-6),45)
 	self.FileName = level_name
 	self.ReferencePath = "Vault\\SceneConfigs\\gp_hangar\\" + level_name + "_gp_hangar_VFS"
 
 
-func add_barrier_wall(object_array, position, orientation_x, orientation_z):
+func add_barrier_wall(object_array, position: Vector3, yaw_degrees: float):
 	var barrier_wall_object = HangarObjects.create_new_object("BarrierWall")
 	var barrier_wall_element : ObjectElement = barrier_wall_object.Elements[0]
 	barrier_wall_element.PosOffset = position
-	barrier_wall_element.OrientationUp = Vector3(0,1,0)
-	#Todo: Figure out how Orientation works
-	barrier_wall_element.OrientationForward = Vector3(orientation_x,0,orientation_z)
+	barrier_wall_element.OrientationUp = Vector3(0, 1, 0)
+	barrier_wall_element.OrientationForward = get_forward_vector_from_yaw_degrees(yaw_degrees)
 	object_array.append(barrier_wall_object)
 
+func get_forward_vector_from_yaw_degrees(yaw_degrees: float) -> Vector3:
+	var radians = deg_to_rad(yaw_degrees)
+	return Vector3(sin(radians), 0, cos(radians)).normalized()
+	
 func as_dict():
 	var dict : Dictionary = {}
 	for var_name in variables_to_json:
