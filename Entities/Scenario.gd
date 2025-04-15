3c8c6be3e8497de558b7be5aa06beca50565554d
jan-laepple <jan.laepple@gmail.com>
2025-04-15 19:26:48 +0200

    file gets saved correctly in gp_hangar dir

--- Entities/Scenario.gd
+++ Entities/Scenario.gd
@@ -1,8 +1,8 @@
 extends Node
 class_name Scenario
 
-var FileName : String = "TEST BOUNDARY2" ##the name of the file and the level
-var ReferencePath : String = "Vault\\SceneConfigs\\gp_hangar\\TEST BOUNDARY_gp_hangar_VFS.json"
+var FileName : String = "Json_Output_test" ##the name of the file and the level
+var ReferencePath : String = "Vault\\SceneConfigs\\gp_hangar\\Json_Output_test_gp_hangar_VFS"
 var Creator : String = "CreatorNameHere"
 var ModsUsed : Array = []
 
@@ -16,8 +16,7 @@ var variables_to_json : Dictionary[String, String] = {
 }
 
 
-func _init(dict : Dictionary):
-	
+func _init(level_name,dict : Dictionary):
 	for variable_name in variables_to_json:
 		var json_value = dict.get(variable_name)
 		set(variable_name, json_value)
@@ -26,15 +25,20 @@ func _init(dict : Dictionary):
 	Objects = HangarObject.generateHangarObjectsFromArray(objects)
 	#Objects = HangarObject.new(objects)
 	var properties = get_property_list()
-	var new_barrier_wall = create_barrier_wall(Vector3(12,1,-6),Vector3(0,1,0))
-	Objects.append(new_barrier_wall)
+	add_barrier_wall(Objects, Vector3(11,0,-6),0,0)
+	add_barrier_wall(Objects, Vector3(10,0,-5),1,1)
+	self.FileName = level_name
+	self.ReferencePath = "Vault\\SceneConfigs\\gp_hangar\\" + level_name + "_gp_hangar_VFS"
 
-func create_barrier_wall(position, orientation):
+
+func add_barrier_wall(object_array, position, orientation_x, orientation_z):
 	var barrier_wall_object = HangarObjects.create_new_object("BarrierWall")
 	var barrier_wall_element : ObjectElement = barrier_wall_object.Elements[0]
 	barrier_wall_element.PosOffset = position
-	barrier_wall_element.OrientationForward = orientation
-	return barrier_wall_object
+	barrier_wall_element.OrientationUp = Vector3(0,1,0)
+	#Todo: Figure out how Orientation works
+	barrier_wall_element.OrientationForward = Vector3(orientation_x,0,orientation_z)
+	object_array.append(barrier_wall_object)
 
 func as_dict():
 	var dict : Dictionary = {}
--- main.gd
+++ main.gd
@@ -7,18 +7,24 @@ var output_file_path = "res://Json_Output_test.json"
 
 
 func _ready():
+	var level_name = "Json_Output_test"
 	var file = FileAccess.open(file_path, FileAccess.READ)
 	var string = file.get_as_text()
 	var json_result = JSON.parse_string(string)
 	print(json_result)
-	var scenario : Scenario = json_to_scenario(json_result)
+	var scenario : Scenario = json_to_scenario(level_name, json_result)
 	var json_string = scenario_to_json(scenario)
+	var output_file_name = level_name + "_gp_hangar_VFS.json"
+	var output_dir = get_h3vr_config_path()
+	var output_file_path = output_dir + output_file_name
+	
 	var output_file = FileAccess.open(output_file_path,FileAccess.WRITE)
 	output_file.store_string(json_string)
+	get_tree().quit()
 	
 
-func json_to_scenario(dict : Dictionary) -> Scenario:
-	var scenario = Scenario.new(dict)
+func json_to_scenario(level_name, dict : Dictionary) -> Scenario:
+	var scenario = Scenario.new(level_name, dict)
 	return scenario
 
 func scenario_to_json(scenario : Scenario) -> String:
@@ -26,3 +32,8 @@ func scenario_to_json(scenario : Scenario) -> String:
 	var json_string = JSON.stringify(dict, "\t", false)
 	return json_string
 	
+
+func get_h3vr_config_path() -> String:
+	var user_profile = OS.get_environment("USERPROFILE") # C:\Users\YourName
+	var path = user_profile + "/Documents/My Games/H3VR/Vault/SceneConfigs/gp_hangar/"
+	return path
